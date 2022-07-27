06cfd748dd99cf3ac69dd87b955aeda354557f2e
alperen.demirdelen@oyakyatirim.com.tr <alperen.demirdelen@oyakyatirim.com.tr>
2022-07-27 15:41:52 +0300

    First Comment

--- RabbitMQDemo.publisher/Program.cs
+++ RabbitMQDemo.publisher/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using System.Text;
+
+
+
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fefpicsr:hi8rgpne3tFVJKaHs0hftBCnIx3mNfzh@cow.rmq2.cloudamqp.com/fefpicsr");
+
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "Hello World!!";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+
+channel.BasicPublish(string.Empty, "hello-queue", null, messageBody); // kuyrukraki isim verilmeli!!!!!!
+
+Console.WriteLine("Mesaj gönderildi");
--- RabbitMQDemo.subscriber/Program.cs
+++ RabbitMQDemo.subscriber/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fefpicsr:hi8rgpne3tFVJKaHs0hftBCnIx3mNfzh@cow.rmq2.cloudamqp.com/fefpicsr");
+
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue",true,consumer);
+
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj:" + message);
+};
+
+Console.ReadLine(); 
